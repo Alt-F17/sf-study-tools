15e2a427036803ecbf9452973ca028a76a1aaf89
Felix Egan <felix.egan+git@icloud.com>
2025-05-22 22:44:17 -0400

    spotthebug build

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -27,7 +27,7 @@ const Index = () => {
     { id: "concept-test", name: "SF2 ConcepTests", component: ConceptTests },
     { id: "stack-queue", name: "Stack or Queue?", component: StackOrQueue },
     { id: "pandas-playground", name: "Pandas and Matplotlib", component: PandasPlayground },
-    { id: "spot-bug", name: "Spot the Bug [WIP]", component: SpotBug },
+    { id: "spot-bug", name: "Spot the Bug", component: SpotBug },
     { id: "list-pattern", name: "Final Boss Problems", component: SuperHardProblems },
     { id: "oop-diagram", name: "OOP Inheritance Diagram", component: OOPDiagram },
     { id: "sort-animator", name: "Step-by-Step Animator", component: StepByStepAnimator },
